d744f6b8934ac6f5b77722e5d914b5ca9b11487a
Patrik Sundberg <patrik.sundberg@gmail.com>
2013-07-24 09:23:50 +0000

    Stamp a release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tulos/lo-sdk "4.0.0-SNAPSHOT"
+(defproject tulos/lo-sdk "4.0.0"
   :description "Wrapping up the LibreOffice java SDK jars into a lein project."
   :url "http://github.com/tulos/lo-sdk/"
   :license {:name "Various"
